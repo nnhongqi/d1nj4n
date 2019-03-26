7ca3890f9512902006fbf1b48ee7732177d95f45
nnhongqi <nn13977169235@163.com>
2019-03-26 17:46:43 +0800

    fgf

--- latte-annotations/src/main/java/com/flj/latte/annotations/EntryGenerator.java
+++ latte-annotations/src/main/java/com/flj/latte/annotations/EntryGenerator.java
@@ -5,7 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * Created by 傅令杰 on 2017/4/2
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface EntryGenerator {
